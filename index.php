aa5e12cfed00cd45566a20b38683fb56c05164c9
kevinvaldes3 <kevinvaldes3@gmail.com>
2017-11-16 00:22:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ZapaColombia</title>
+	<!--<link href="css/Carousel.css" rel="stylesheet">-->
+	<link href="repo/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!--<link rel="stylesheet" href="../css/estilos.css" />-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="repo/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../repo/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <!-- Plugin CSS -->
+    <link href="../repo/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.css" rel="stylesheet">
+
+</head>
+<body id="page-top">
+ <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top"  id="mainNav">
+      <div class="container">
+        <img src="img/huella3.png" class="navbar-brand js-scroll-trigger" width="300px" height="100px"> </a>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#Acerca">Acerca del producto </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portafolio">Portafolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contactenos">Contactenos</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" id="nav-4" href="#contact">Preguntas Frecuentes </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <header class="masthead">
+      <div class="header-content">
+        <div class="header-content-inner">
+          <h1 id="homeHeading">Lo mejor en el calzado colombiano </h1>
+          Hechos en colombia para colombia</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#Acerca">Mas Información</a>
+        </div>
+      </div>
+    </header>
+    <section class="bg-primary" id="Acerca">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">¡Calza Colombia!</h2>
+            <hr class="light">
+            <p class="text-faded">Calza Colombia una empresa dedica a calzar a todos los colombianos, con los mejores diseños y materiales, para todo tipo de ocación. ¡ Ey que esperas para calzar los tuyos!</p>
+
+            <div class="embed-responsive embed-responsive-21by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/IS6n2Hx9Ykk?rel=0" allowfullscreen></iframe>
+            </div>
+            <br><br>
+            <div class="row justify-content-md-center">
+              <a class="btn btn-default btn-xl js-scroll-trigger" href="#portafolio">Empezar para calzar !</a>
+            </div>  
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="p-0" id="portafolio">
+      <div class="container-fluid">
+        <div class="row no-gutter popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+           <a class="portfolio-box"> <!--  href="img/portfolio/fullsize/1.jpg"-->
+              <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Deportivo
+                  </div>
+                  <div class="project-name">
+                    
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" ><!--href="img/portfolio/fullsize/2.jpg" -->
+              <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Casual
+                  </div>
+                  <div class="project-name">
+                    
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" ><!-- href="img/portfolio/fullsize/3.jpg"-->
+              <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Formal
+                  </div>
+                  <div class="project-name">
+                    
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" "><!-- href="img/portfolio/fullsize/4.jpg-->
+              <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Alpinismo
+                  </div>
+                  <div class="project-name">
+                    
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" ><!--href="img/portfolio/fullsize/5.jpg" -->
+              <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Motociclista
+                  </div>
+                  <div class="project-name">
+                    
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" ><!--href="img/portfolio/fullsize/6.jpg"-->
+              <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    TRabajo pesado
+                  </div>
+                  <div class="project-name">
+                   
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-primary"" id="contactenos">
+      <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">Contactenos</h2>
+          </div>
+      <form>
+            <div class="form-group row">
+              
+            </div>
+            <div class="form-group row">
+                  <label class="col-md-5 text-center" id="label" >Nombre:</label>
+                        <div class="col-md-6">
+                             <input value="Nombre" class="form-control" type="text">
+                        </div>      
+            </div>
+            <div class="form-group row">
+                  <label class="col-md-5 text-center" id="label" >Correo Electronico:</label>
+                        <div class="col-md-6">
+                             <input value="Correo" class="form-control" type="text">
+                        </div>      
+            </div>
+             <div class="form-group row">
+                  <label class="col-md-5 text-center" id="label" >Asunto:</label>
+                        <div class="col-md-6">
+                             <input value="Asunto" class="form-control" type="text">
+                        </div>      
+            </div>
+             <div class="form-group row">
+                  <label class="col-md-5 text-center" id="label" >Mensaje:</label>
+                        <div class="col-md-6">
+                             <textarea value="Mensaje" class="form-control" type="text"> </textarea> 
+                        </div>      
+            </div>
+            <div class="row justify-content-md-center"> 
+              <button type="button" class="btn text-center  col-md-3 btn-secondary">Enviar</button>
+            </div>
+            
+      </form>
+    </section>
+
+</body>
+<!-- Bootstrap core JavaScript -->
+    <script src="repo/jquery/jquery.min.js"></script>
+    <script src="repo/popper/popper.min.js"></script>
+    <script src="repo/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="repo/jquery-easing/jquery.easing.min.js"></script>
+    <script src="repo/scrollreveal/scrollreveal.min.js"></script>
+    <script src="repo/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+    <script type="text/javascript">
+    	
+    	$('.portfolio-box').click(function()
+		{
+			//$('.info_20').show();
+			//$( '.info_20' ).html("Precio del Producto 200.000 30 porciones, incluye servicio a domicilio");
+			alert("Estamos cos para ti, nuestros productos");
+		});
+      $('#nav-4').click(function()
+        {
+          alert('Estamos cos tu sesion');
+        });
+    </script>
+</html>
